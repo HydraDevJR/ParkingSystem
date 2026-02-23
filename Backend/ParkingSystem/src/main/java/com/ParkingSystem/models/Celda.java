e6ae991c1a08aa3a051fbd8fd2e2b8250e2929fd
HydraDevJR <jorgelrg18@gmail.com>
2026-02-23 09:48:34 -0500

    Add entity classes for Acceso, Celda, Cuenta, CuentaRol, and Estadia with appropriate fields and annotations (Jorge)

--- Backend/ParkingSystem/src/main/java/com/ParkingSystem/models/Celda.java
+++ Backend/ParkingSystem/src/main/java/com/ParkingSystem/models/Celda.java
@@ -1,5 +1,13 @@
 package com.ParkingSystem.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "celdas")
 public class Celda {
 
     public enum EstadoCelda {
@@ -7,5 +15,73 @@ public class Celda {
         Ocupada,
         Reservada
     }
-    //id, numero, piso, tipo(carro, moto...), estado(disponible, ocupada, reservada), parqueadero
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer numero;
+    private Integer piso;
+    private TipoVehiculo tipo;
+    private EstadoCelda estado;
+    private Parqueadero parqueadero;
+
+    public Celda(Integer id, Integer numero, Integer piso, TipoVehiculo tipo, EstadoCelda estado, Parqueadero parqueadero) {
+        this.id = id;
+        this.numero = numero;
+        this.piso = piso;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.parqueadero = parqueadero;
+    }
+
+    public Celda() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Integer getPiso() {
+        return piso;
+    }
+
+    public void setPiso(Integer piso) {
+        this.piso = piso;
+    }
+
+    public TipoVehiculo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoVehiculo tipo) {
+        this.tipo = tipo;
+    }
+
+    public EstadoCelda getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCelda estado) {
+        this.estado = estado;
+    }
+
+    public Parqueadero getParqueadero() {
+        return parqueadero;
+    }
+
+    public void setParqueadero(Parqueadero parqueadero) {
+        this.parqueadero = parqueadero;
+    }
+
 }
