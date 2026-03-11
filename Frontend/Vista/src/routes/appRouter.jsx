4678942dfde1a5aaece5c75df1b6b33403bf762e
HydraDevJr <jorgelrg18@gmail.com>
2026-03-11 08:27:04 -0500

    Configuracion router

---
+++ Frontend/Vista/src/routes/appRouter.jsx
@@ -0,0 +1,12 @@
+//Hacer import de los componentes a usar en las rutas
+
+export let router = [ // Rutas para pages
+    {
+        path: "",
+        element: 
+    },
+    {
+        path: "",
+        element:
+    }
+]
