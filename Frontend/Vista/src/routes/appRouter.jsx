cf5defdb61c590bcfe478d74188abc55e28b4349
HydraDevJr <jorgelrg18@gmail.com>
2026-03-11 08:40:06 -0500

    Se importa el componente App a router para navegacion

--- Frontend/Vista/src/routes/appRouter.jsx
+++ Frontend/Vista/src/routes/appRouter.jsx
@@ -1,6 +1,11 @@
 //Hacer import de los componentes a usar en las rutas
+import App from "../App"
 
 export let router = [ // Rutas para pages
+    {
+        path: "/",
+        element: <App />
+    },
     {
         path: "",
         element: 
