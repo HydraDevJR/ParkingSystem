71114e82490017c9a4ada8eea3adc856893a2918
pipejaramillo913-del <pipejaramillo913@gmail.com>
2026-04-07 16:15:49 -0500

    Tarifa todo al dia

---
+++ Backend/ParkingSystem/src/main/java/com/ParkingSystem/repositories/ITarifaRepositorie.java
@@ -0,0 +1,25 @@
+package com.ParkingSystem.repositories;
+
+import com.ParkingSystem.models.Tarifa;
+import com.ParkingSystem.models.Tarifa.TipoTarifa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ITarifaRepositorie extends JpaRepository<Tarifa, Long> {
+
+    
+    List<Tarifa> findByActivoTrue();
+
+
+    Optional<Tarifa> findByTipo(TipoTarifa tipo);
+
+    
+    Optional<Tarifa> findByTipoAndActivoTrue(TipoTarifa tipo);
+
+    
+    List<Tarifa> findByValorLessThanEqual(double valor);
+}
