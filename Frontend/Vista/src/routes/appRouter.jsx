025c54fafa266fff8f88d8e97503d2d93ce751c0
HydraDevJr <jorgelrg18@gmail.com>
2026-04-15 17:57:36 -0500

    (chore): Se han modificado varios archivos

--- Frontend/Vista/src/routes/appRouter.jsx
+++ Frontend/Vista/src/routes/appRouter.jsx
@@ -1,13 +1,10 @@
 //Hacer import de los componentes a usar en las rutas
 import App from "../App"
+import Login from "../pages/Login"
 
 export let router = [ // Rutas para pages
     {
         path: "/",
-        element: <App />
-    },
-    {
-        path: "/Frontend/Vista/src/App.jsx",
-        element: <App />
+        element: <Login />
     }
 ]
