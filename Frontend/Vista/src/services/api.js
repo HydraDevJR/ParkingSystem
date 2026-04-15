16039d4b337b03493df3bbff9ebee39fee333531
HydraDevJr <jorgelrg18@gmail.com>
2026-04-15 17:55:22 -0500

    (feat): Se agrega archivo api.js donde se va a configurar el llamado a la API

---
+++ Frontend/Vista/src/services/api.js
@@ -0,0 +1,9 @@
+const URL_BASE = "https://app-gastos-personales-api.onrender.com" + "/"
+
+export let urlAPI = {
+    users: URL_BASE + "users/", /* localhost:8080/users/ */
+    merchants: URL_BASE + "merchants/",
+    expenses: URL_BASE + "expenses/",
+    payment_methods: URL_BASE + "payment_methods/",
+    category: URL_BASE + "category/"
+}
