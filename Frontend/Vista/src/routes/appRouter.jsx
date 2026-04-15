df8989a0a1b9c437639a97d0238225b86b0faecf
HydraDevJr <jorgelrg18@gmail.com>
2026-04-15 18:21:32 -0500

    (feat): Se agrega componente Register y su configuración en appRouter

--- Frontend/Vista/src/routes/appRouter.jsx
+++ Frontend/Vista/src/routes/appRouter.jsx
@@ -1,10 +1,15 @@
 //Hacer import de los componentes a usar en las rutas
 import App from "../App"
 import Login from "../pages/Login"
+import Register from "../pages/Register"
 
 export let router = [ // Rutas para pages
     {
         path: "/",
         element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
     }
 ]
