8f5a0689bcf85760d114721ef689bc65a882ab62
HydraDevJr <jorgelrg18@gmail.com>
2026-04-27 08:45:26 -0500

    chore: Desarrollo de repositorio vehiculo

--- Backend/ParkingSystem/src/main/java/com/ParkingSystem/repositories/IVehiculoRepositorie.java
+++ Backend/ParkingSystem/src/main/java/com/ParkingSystem/repositories/IVehiculoRepositorie.java
@@ -1,9 +1,24 @@
 package com.ParkingSystem.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ParkingSystem.models.Vehiculo;
 
+@Repository
 public interface IVehiculoRepositorie extends JpaRepository<Vehiculo, Integer> {
     
+    //Buscar por placa exacta (1)
+    public Vehiculo findByPlaca(String placa);
+
+    //Buscar por marca exacta (lista)
+    public List<Vehiculo> findByMarca(String marca);
+
+    //Buscar por placa que contenga nnn (lista)
+    public List<Vehiculo> findByPlacaContaining(String placa);
+
+    //Buscar por modelo (lista)
+    public List<Vehiculo> findByModelo(String modelo);
 }
