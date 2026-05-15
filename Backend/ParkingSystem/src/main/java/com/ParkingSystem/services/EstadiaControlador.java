49e066c507538d8860cf76b745b56e8ea9ee4284
pinedajuanmanuel706-coder <pinedajuanmanuel706@gmail.com>
2026-05-15 12:07:31 +0000

    eliminar ,buscar y modificar en estadia service y controlador estadia

--- Backend/ParkingSystem/src/main/java/com/ParkingSystem/services/EstadiaControlador.java
+++ Backend/ParkingSystem/src/main/java/com/ParkingSystem/services/EstadiaControlador.java
@@ -19,22 +19,44 @@ public class EstadiaControlador {
     //para cada servicio ofrecido se debe programar una funcion
     //esa funcion recibira las peticiones del pedido y respondera
 
-    //funcion controladora del servicio guardar estadia
+    //Funcion controladora del servicio de guardar una estadia
     @PostMapping
-    public ResponseEntity<?> controladorGuardar(@RequestBody estadia datos){
-        return ResponseEntity.status(HttpStatus.ok).body(
-            servicio.guardarEstadia(datos)
+    public ResponseEntity<?> controllerGuardar(@RequestBody estadia estadia){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            servicio.guardarEstadia(estadia)
         );
     }
-    
 
-    //funcion controladora del servicio de listar todos los usuarios
+    //Funcion controladora del servicio de listar todas las estadias
     @GetMapping
-    public ResponseEntity<?>controladorListarTodo(){
-        return ResponseEntity.status(HttpStatus.ok).body(
+    public ResponseEntity<?> controllerListar(){
+        return ResponseEntity.status(HttpStatus.OK).body(
             servicio.listarEstadias()
         );
     }
+
+    //funcion controladora del servicio modificar
+    @PutMapping("/{id}")
+    public ResponseEntity<?> controladorModificar(@PathVariable Integer id, @RequestBody estadia datos){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            servicio.modificarEstadia(id,datos)
+        );
+    }
+    //funcion controladora del servicio eliminar 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> controladorEliminar(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            servicio.eliminarEstadia(id)
+        );
+    }
+    //funcion controladora del servicio buscar por id
+    @GetMapping("/{id}")
+    public ResponseEntity<?> controladorBuscarporId(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            servicio.buscarEstadiaPorId(id)
+        );
+    }
+
 
 
 }
