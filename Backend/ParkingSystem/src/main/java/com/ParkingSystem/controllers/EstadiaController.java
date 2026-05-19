2c2af506f5d43037f1b05b400792f60395077456
dj107-il <diegoballes4@gmail.com>
2026-05-19 17:07:05 -0500

    feat(estadia): ajustar servicio, model, repositorio  y controlador para busqueda por ID, eliminar, modificar y correciones de Estadia

--- Backend/ParkingSystem/src/main/java/com/ParkingSystem/controllers/EstadiaController.java
+++ Backend/ParkingSystem/src/main/java/com/ParkingSystem/controllers/EstadiaController.java
@@ -1,8 +1,11 @@
 package com.ParkingSystem.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,49 +21,63 @@ public class EstadiaController {
     
     //Inyectar el servicio correspondiente
     @Autowired
-    private EstadiaService servicio;
+    private EstadiaService estadiaService;
 
     //para cada servicio ofrecido se debe programar una funcion
     //esa funcion recibira las peticiones del pedido y respondera
 
     //Funcion controladora del servicio de guardar una estadia
     @PostMapping
-    public ResponseEntity<?> controladorGuardar(@RequestBody estadia datos){
-        return ResponseEntity.status(HttpStatus.ok).body(
-            servicio.guardarEstadia(datos)
+    public ResponseEntity<?> controladorGuardar(@RequestBody Estadia datos){
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            estadiaService.guardarEstadia(datos)
         );
     }
 
     //Funcion controladora del servicio de listar todas las estadias
     @GetMapping
-    public ResponseEntity<?>controladorListarTodo(){
-        return ResponseEntity.status(HttpStatus.ok).body(
-            servicio.listarEstadias()
+    public ResponseEntity<?> controladorListarTodo() {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            estadiaService.listarEstadias()
         );
     }
 
     //funcion controladora del servicio modificar
     @PutMapping("/{id}")
-    public ResponseEntity<?> controladorModificar(@PathVariable Integer id, @RequestBody estadia datos){
+    public ResponseEntity<?> controladorModificar(@PathVariable Integer id, @RequestBody Estadia datosActualizados){
         return ResponseEntity.status(HttpStatus.OK).body(
-            servicio.modificarEstadia(id,datos)
+            estadiaService.actualizarEstadia(id,datosActualizados)
         );
     }
+    
     //funcion controladora del servicio eliminar 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> controladorEliminar(@PathVariable Integer id){
         return ResponseEntity.status(HttpStatus.OK).body(
-            servicio.eliminarEstadia(id)
+            estadiaService.eliminarEstadia(id)
         );
     }
-    //funcion controladora del servicio buscar por id
+    //funcion controladora del servicio buscar por id la estadia
     @GetMapping("/{id}")
     public ResponseEntity<?> controladorBuscarporId(@PathVariable Integer id){
         return ResponseEntity.status(HttpStatus.OK).body(
-            servicio.buscarEstadiaPorId(id)
+            estadiaService.obtenerEstadiaById(id)
         );
     }
 
+    //función controladora del servicio buscar estadia por vehiculo
+    @GetMapping("/vehiculo/{id}")
+    public ResponseEntity<?> controladorBuscarPorVehiculo(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            estadiaService.obtenerEstadiasActivasPorVehiculo(id)
+        );
+    }
 
-
+    //Función controladora buscar estadia por celda
+    @GetMapping("/celda/{id}")
+    public ResponseEntity<?> controladorBuscarPorCelda(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(
+            estadiaService.obtenerEstadiasPorCelda(id)
+        );
+    }
 }
